1697ecab675b4f2f69f4eb8ce3ddbd370ec3853f
likkikop <75980548+likkikop@users.noreply.github.com>
2024-07-13 03:27:04 +0530

    Update setup.sh

--- streamlit-app/setup.sh
+++ streamlit-app/setup.sh
@@ -1,9 +1,4 @@
-mkdir -p ~/.streamlit/
-
-echo "\
-[server]\n\
-port = $PORT\n\
-enableCORS = false\n\
-headless = true\n\
-\n\
-" > ~/.streamlit/config.toml
+[server]
+port = $PORT
+enableCORS = false
+headless = true
